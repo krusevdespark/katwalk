bef0685f50cca6e98af3a57535c69f6506be53c3
Kaloyan Rusev <krusev@despark.com>
2014-03-17 13:30:02 +0200

    test1

--- rggarb/src/net/shiftinpower/core/C.java
+++ rggarb/src/net/shiftinpower/core/C.java
@@ -9,7 +9,7 @@ package net.shiftinpower.core;
  * 
  */
 
-public final class C {
+public final class C { //asd
 
 	public static final class Preferences {
 		// Until the Landscape version of the App is developed, we enforce Portrait Mode
